c6bc204430542437a0e7878b01d4221ca3e0716f
DESKTOP-8MACDP5\user <onurcandedelioglu10@gmail.com>
2021-08-26 12:08:20 +0300

    Anca oldu :D

---
+++ detailspage/details.js
@@ -0,0 +1,239 @@
+let baseEndpoint = "https://api.tvmaze.com";
+let params = new URLSearchParams(window.location.search);
+
+let isTvShows = params.has('id');
+if(isTvShows){
+    let seriesId = params.get('id');
+    showDetail(seriesId);
+}
+
+let selectBox = document.querySelector('[data-select]');
+
+
+function showDetail(id){
+
+    //SHOW DETAILS
+
+    let detailsEndpoint = `${baseEndpoint}/shows/${id}`;
+
+    fetch(detailsEndpoint)
+    .then(response => {
+        if(!response.ok) {
+            throw "There were no results";
+        }
+        return response.json();
+    })
+    .then(response => {
+        let seriesImage = response.image.medium;
+        let img = document.querySelector('[data-image]');
+        img.src= seriesImage;
+
+        let seriesName = response.name;
+        let name = document.querySelector('[data-name]');
+        name.innerHTML = seriesName.toUpperCase();
+
+        let summary = response.summary;
+        let subject = document.querySelector('[data-subject]');
+        subject.innerHTML = summary;
+    }).catch(error => {
+        let subject = document.querySelector('[data-subject]');
+        subject.innerHTML = `This Tv Show Faulty.`;
+    })
+
+    //CAST
+
+    let castEndpoint = `${baseEndpoint}/shows/${id}/cast`;
+    
+    fetch(castEndpoint)
+    .then(response => {
+        if(!response.ok) {
+                throw "There were no results";
+            }
+        return response.json();
+    })
+    .then(response => {
+        let castParent = document.querySelector('[data-cast]');
+
+        let cast = response;
+        let count = 0;
+        cast.forEach(item => {
+            if(count > 5) return false;
+            let castImage = item.person.image.medium;
+            let castName = item.person.name;
+            let characterName = item.character.name;
+
+            let castAside = `
+                <div class="details__info">
+                    <img class="details__castimage" src="${castImage}">
+                    <p class="details__castname">${castName}</p>
+                    <p class="details__charactername">${characterName}</p>
+                </div>
+            `
+
+            castParent.insertAdjacentHTML("beforeend", castAside);
+            count++;
+        });
+    }).catch(error => {
+        let castParent = document.querySelector('[data-cast]');
+        let errorHandle = `
+        <p class="error__handle">Cast Not Found.</p>
+        `;
+        castParent.insertAdjacentHTML("beforeend", errorHandle);
+    })
+
+    //SEASON
+
+    let seasonEndpoint = `${baseEndpoint}/shows/${id}/seasons`;
+    
+    fetch(seasonEndpoint)
+    .then(response => {
+        if(!response.ok) {
+                throw "There were no results";
+            }
+        return response.json();
+    })
+    .then(response => {
+        let seasonParent = document.querySelector('[data-season]');
+
+        let season = response;
+
+        season.forEach(item => {
+            let number = item.number;
+            let premiereDate = item.premiereDate;
+            let seasonId = item.id;
+
+            let seasonAside = `
+                <button data-button data-season-number="${number}" data-season-episode-id="${seasonId}" class="season__button">
+                    <span class="season__number"> Season ${number}</span>
+                    <span class="season__date">${premiereDate}</span>
+                </button>
+            `
+            seasonParent.insertAdjacentHTML("beforeend", seasonAside);
+
+            let selectBoxOption = `
+                <option data-option class="season__option" select-season-number="${number}" select-season-episode-id="${seasonId}">Season ${number}
+                </option>
+            `  
+            selectBox.insertAdjacentHTML("beforeend",selectBoxOption);
+
+        });
+        let seasonButton = document.querySelectorAll('[data-button]');
+        seasonButton.forEach(button => {
+            button.addEventListener('click', selectSeason);
+        });
+        seasonButton[0].classList.add("-selectedSeason");
+        getSummary(season[0].id, season[0].number);
+        getEpisodes(season[0].id);
+    }).catch(error => {
+        let seasonParent = document.querySelector('[data-season]');
+        let errorHandle = `
+        <p class="error__handle">Season Not Found.</p>
+        `;
+        seasonParent.insertAdjacentHTML("beforeend", errorHandle);
+    })
+}
+
+function selectSeason(event){
+    let seasonSection = document.querySelector("[data-scroll]");
+    seasonSection.scrollIntoView(true)
+    let buttons = document.querySelectorAll('[data-button]');
+    buttons.forEach(item => {
+        item.classList.remove("-selectedSeason");
+    })
+
+    let pressedButton = event.target;
+    pressedButton.classList.add("-selectedSeason");
+
+    let seasonNumber = pressedButton.getAttribute('data-season-number');
+    let seasonNumberItem = document.querySelector('[data-season-episode]');
+    seasonNumberItem.innerHTML = `Season ${seasonNumber}`;
+
+    let seasonEpisodeId = pressedButton.getAttribute('data-season-episode-id');
+
+    getSummary(seasonEpisodeId, seasonNumber);
+    getEpisodes(seasonEpisodeId);
+
+    let episodeSeason = document.querySelectorAll('[data-episode-aside]');
+        episodeSeason.forEach(item => {
+            item.remove();
+    })
+}
+
+
+function getSummary(seasonEpisodeId, seasonNumber){
+    let seasonEndpoint = `${baseEndpoint}/seasons/${seasonEpisodeId}`;
+    let summaryItem = document.querySelector('[data-summary]');
+
+    let seasonNumberItem = document.querySelector('[data-season-episode]');
+    seasonNumberItem.innerHTML = `Season ${seasonNumber}`;
+    
+    fetch(seasonEndpoint)
+    .then(response => {
+        if(!response.ok) {
+                throw "There were no results";
+            }
+        return response.json();
+    })
+    .then(response => {
+        summaryItem.innerHTML = response.summary;
+    }).catch(error => {
+        let summaryItem = document.querySelector('[data-summary]');
+        summaryItem.innerHTML = `Season Summary Not Found.`;
+    })
+}
+
+function getEpisodes(seasonEpisodeId){
+
+    let seasonEpisodesEndpoint = `${baseEndpoint}/seasons/${seasonEpisodeId}/episodes`;
+    fetch(seasonEpisodesEndpoint)
+    .then(response => {
+        if(!response.ok) {
+                throw "There were no results";
+            }
+        return response.json();
+    })
+    .then(response => {
+        let episodeParent = document.querySelector('[data-episode]');
+
+        let episode = response;
+
+        let errorsEpisodes = document.querySelectorAll('[data-error]');
+        errorsEpisodes.forEach(item => {
+            item.remove();
+        })
+        episode.forEach(item => {
+            let episodeNumber = item.number;
+            let episodeName = item.name;
+            let episodeImage = item.image.medium;
+
+            let episodeAside = `
+                <div data-episode-aside=${episodeNumber} class="season__episodeaside">
+                    <h2 class="season__episodenumber">Episode ${episodeNumber}</h2>
+                    <h2 class="season__episodename">${episodeName}</h2>
+                </div>
+            `
+            episodeParent.insertAdjacentHTML("beforeend", episodeAside);
+            let episodeItem = document.querySelector(`[data-episode-aside="${episodeNumber}"]`);
+            episodeItem.style.backgroundImage = `url(${episodeImage}`;
+        });
+    }).catch(error => {
+        let episodeParent = document.querySelector('[data-episode]');
+        let errorHandle = `
+        <p data-error class="error__handle">Episodes Not Found.</p>
+        `
+        episodeParent.insertAdjacentHTML("beforeend", errorHandle);
+    })
+}
+function selectBoxSeason(event){
+    let seasonIndex = selectBox.options[selectBox.selectedIndex];
+    let seasonId = seasonIndex.getAttribute("select-season-number");
+    let seasonEpisodeId = seasonIndex.getAttribute("select-season-episode-id");
+    getSummary(seasonEpisodeId, seasonId);
+    getEpisodes(seasonEpisodeId);
+    let episodeSeason = document.querySelectorAll('[data-episode-aside]');
+        episodeSeason.forEach(item => {
+            item.remove();
+    })
+}
+selectBox.addEventListener('change', selectBoxSeason);
+
---
+++ homepage/popular.js
@@ -0,0 +1,83 @@
+let baseEndpoint = "https://api.tvmaze.com";
+
+let imageUrlId = [335,66,44,123,43677];
+imageUrl();
+
+function imageUrl(){
+    for(let index = 0; index < 5; index++){
+        let imageLinkItem = document.querySelectorAll(`[data-link="${index}"]`);
+        imageLinkItem.forEach(item => {
+            item.href = `./details.html?id=${imageUrlId[index]}`;
+        })
+    }
+}
+
+popularTvSeries();
+function popularTvSeries(){
+    let showsEndpoint = `${baseEndpoint}/shows`;
+    
+    fetch(showsEndpoint)
+    .then(response => {
+        if(!response.ok) {
+                throw "There were no results";
+            }
+        return response.json();
+    })
+    .then(response => {
+        response = response.sort((responseFirstItem, responseSecondItem) => {
+
+            let tvSeries = responseFirstItem.rating;
+            let firstImdb = Object.values(tvSeries);
+
+            let secondTvSeries = responseSecondItem.rating;
+            let secondImdb = Object.values(secondTvSeries);
+
+            if(firstImdb[0] == null) firstImdb[0] = 0;
+            if(secondImdb[0] == null) secondImdb[0] = 0;
+
+            return parseFloat(firstImdb) - parseFloat(secondImdb);
+        })
+        let topTenSeries = response.slice(Math.max(response.length - 10, 1));
+        showPopularImage(topTenSeries)
+    }).catch(error => {
+        alert(`Failed To Sort Popular List`);
+    })
+}
+function showPopularImage(topTen){
+    for(let i = 0; i < topTen.length; i++){
+
+        let series = topTen[i];
+
+        let popularCard = document.querySelector(`[data-popular="${i}"]`);
+        let cardImage = popularCard.querySelector('[data-image]');
+        let mediumImage = series.image.medium;
+        cardImage.src = mediumImage;
+
+        let colorthief = new ColorThief();
+        cardImage = popularCard.querySelector('[data-image]');
+        cardImage.addEventListener('load',function(){
+            let rgb = colorthief.getColor(cardImage)
+            let red = rgb[0];
+            let green = rgb[1];
+            let blue = rgb[2];
+            let cardDetails = popularCard.querySelector('[data-popular-details]');
+            cardDetails.style.backgroundColor = `rgb(${red},${green},${blue})`;
+        })        
+
+        let seriesTitle = popularCard.querySelector('[data-title]');
+        let seriesName = series.name;
+        seriesTitle.innerHTML = seriesName;
+
+        let seriesImdbFind = topTen[i].rating;
+        let seriesAvarage = Object.values(seriesImdbFind);
+        let seriesImdb = popularCard.querySelector('[data-imdb]');
+        seriesImdb.innerHTML = `${seriesAvarage[0]}`;
+
+        let seriesGenresItem = popularCard.querySelector('[data-genres');
+        let seriesGenres = series.genres;
+        seriesGenresItem.innerHTML = seriesGenres
+        
+        let seriesId = series.id;
+        popularCard.href = `./details.html?id=${seriesId}`;
+    }
+}
