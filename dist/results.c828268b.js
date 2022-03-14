309a5bcac8d6f119f5c107860526b0ff0aa2c92a
onurcanDEDELIOGLU <onurcandedelioglu10@gmail.com>
2022-03-14 23:47:20 +0300

    Updated Dist

---
+++ dist/results.c828268b.js
@@ -0,0 +1,408 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"searchpage/results.js":[function(require,module,exports) {
+var baseEndpoint = "https://api.tvmaze.com";
+var searchBar = document.querySelector('[data-menu-input]');
+var secondSearchBar = document.querySelector('[data-input]');
+var searchBarInput = document.querySelector('[data-input-aside]');
+searchBarInput.classList.add('-responsiveSearch');
+var searchBarAside = document.querySelector('[data-search-display]');
+searchBarAside.classList.add('-responsiveSearch');
+
+var searchBarText = function searchBarText(seriesName) {
+  searchBar.value = seriesName;
+  secondSearchBar.value = seriesName;
+};
+
+var hasDetails = function hasDetails(id, image, medium, name) {
+  if (id == null || image == null || medium == null || name == null) return true;
+  return false;
+};
+
+var isNull = function isNull(item) {
+  if (item == null || item.length == 0) return "-";
+  return item;
+};
+
+var resultsItem = document.querySelector('[data-results]');
+var resultContent = document.querySelector('[data-content]');
+
+var showResults = function showResults(seriesName) {
+  var searchEndpoint = "".concat(baseEndpoint, "/search/shows?q=").concat(seriesName);
+  fetch(searchEndpoint).then(function (response) {
+    if (!response.ok) {
+      throw "There were no results";
+    }
+
+    return response.json();
+  }).then(function (response) {
+    var index = 0;
+    response.forEach(function (item) {
+      try {
+        var details = hasDetails(item.show.id, item.show.image, item.show.image.medium, item.show.name);
+        if (details) throw "This Tv Show Faulty";
+        var tvSeriesId = item.show.id;
+        var tvSeriesImage = item.show.image.medium;
+        var tvSeriesName = item.show.name;
+        var tvSeriesGenres = isNull(item.show.genres);
+        var tvSeriesImdb = isNull(item.show.rating.average);
+        var svg = "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" preserveAspectRatio=\"xMidYMid meet\" viewBox=\"0 0 575 289.83\" width=\"575\" height=\"289.83\"><defs><path d=\"M575 24.91C573.44 12.15 563.97 1.98 551.91 0C499.05 0 76.18 0 23.32 0C10.11 2.17 0 14.16 0 28.61C0 51.84 0 237.64 0 260.86C0 276.86 12.37 289.83 27.64 289.83C79.63 289.83 495.6 289.83 547.59 289.83C561.65 289.83 573.26 278.82 575 264.57C575 216.64 575 48.87 575 24.91Z\" id=\"d1pwhf9wy2\"></path><path d=\"M69.35 58.24L114.98 58.24L114.98 233.89L69.35 233.89L69.35 58.24Z\" id=\"g5jjnq26yS\"></path><path d=\"M201.2 139.15C197.28 112.38 195.1 97.5 194.67 94.53C192.76 80.2 190.94 67.73 189.2 57.09C185.25 57.09 165.54 57.09 130.04 57.09L130.04 232.74L170.01 232.74L170.15 116.76L186.97 232.74L215.44 232.74L231.39 114.18L231.54 232.74L271.38 232.74L271.38 57.09L211.77 57.09L201.2 139.15Z\" id=\"i3Prh1JpXt\"></path><path d=\"M346.71 93.63C347.21 95.87 347.47 100.95 347.47 108.89C347.47 115.7 347.47 170.18 347.47 176.99C347.47 188.68 346.71 195.84 345.2 198.48C343.68 201.12 339.64 202.43 333.09 202.43C333.09 190.9 333.09 98.66 333.09 87.13C338.06 87.13 341.45 87.66 343.25 88.7C345.05 89.75 346.21 91.39 346.71 93.63ZM367.32 230.95C372.75 229.76 377.31 227.66 381.01 224.67C384.7 221.67 387.29 217.52 388.77 212.21C390.26 206.91 391.14 196.38 391.14 180.63C391.14 174.47 391.14 125.12 391.14 118.95C391.14 102.33 390.49 91.19 389.48 85.53C388.46 79.86 385.93 74.71 381.88 70.09C377.82 65.47 371.9 62.15 364.12 60.13C356.33 58.11 343.63 57.09 321.54 57.09C319.27 57.09 307.93 57.09 287.5 57.09L287.5 232.74L342.78 232.74C355.52 232.34 363.7 231.75 367.32 230.95Z\" id=\"a4ov9rRGQm\"></path><path d=\"M464.76 204.7C463.92 206.93 460.24 208.06 457.46 208.06C454.74 208.06 452.93 206.98 452.01 204.81C451.09 202.65 450.64 197.72 450.64 190C450.64 185.36 450.64 148.22 450.64 143.58C450.64 135.58 451.04 130.59 451.85 128.6C452.65 126.63 454.41 125.63 457.13 125.63C459.91 125.63 463.64 126.76 464.6 129.03C465.55 131.3 466.03 136.15 466.03 143.58C466.03 146.58 466.03 161.58 466.03 188.59C465.74 197.84 465.32 203.21 464.76 204.7ZM406.68 231.21L447.76 231.21C449.47 224.5 450.41 220.77 450.6 220.02C454.32 224.52 458.41 227.9 462.9 230.14C467.37 232.39 474.06 233.51 479.24 233.51C486.45 233.51 492.67 231.62 497.92 227.83C503.16 224.05 506.5 219.57 507.92 214.42C509.34 209.26 510.05 201.42 510.05 190.88C510.05 185.95 510.05 146.53 510.05 141.6C510.05 131 509.81 124.08 509.34 120.83C508.87 117.58 507.47 114.27 505.14 110.88C502.81 107.49 499.42 104.86 494.98 102.98C490.54 101.1 485.3 100.16 479.26 100.16C474.01 100.16 467.29 101.21 462.81 103.28C458.34 105.35 454.28 108.49 450.64 112.7C450.64 108.89 450.64 89.85 450.64 55.56L406.68 55.56L406.68 231.21Z\" id=\"fk968BpsX\"></path></defs><g><g><g><use xlink:href=\"#d1pwhf9wy2\" opacity=\"1\" fill=\"#f6c700\" fill-opacity=\"1\"></use><g><use xlink:href=\"#d1pwhf9wy2\" opacity=\"1\" fill-opacity=\"0\" stroke=\"#000000\" stroke-width=\"1\" stroke-opacity=\"0\"></use></g></g><g><use xlink:href=\"#g5jjnq26yS\" opacity=\"1\" fill=\"#000000\" fill-opacity=\"1\"></use><g><use xlink:href=\"#g5jjnq26yS\" opacity=\"1\" fill-opacity=\"0\" stroke=\"#000000\" stroke-width=\"1\" stroke-opacity=\"0\"></use></g></g><g><use xlink:href=\"#i3Prh1JpXt\" opacity=\"1\" fill=\"#000000\" fill-opacity=\"1\"></use><g><use xlink:href=\"#i3Prh1JpXt\" opacity=\"1\" fill-opacity=\"0\" stroke=\"#000000\" stroke-width=\"1\" stroke-opacity=\"0\"></use></g></g><g><use xlink:href=\"#a4ov9rRGQm\" opacity=\"1\" fill=\"#000000\" fill-opacity=\"1\"></use><g><use xlink:href=\"#a4ov9rRGQm\" opacity=\"1\" fill-opacity=\"0\" stroke=\"#000000\" stroke-width=\"1\" stroke-opacity=\"0\"></use></g></g><g><use xlink:href=\"#fk968BpsX\" opacity=\"1\" fill=\"#000000\" fill-opacity=\"1\"></use><g><use xlink:href=\"#fk968BpsX\" opacity=\"1\" fill-opacity=\"0\" stroke=\"#000000\" stroke-width=\"1\" stroke-opacity=\"0\"></use></g></g></g></g></svg>";
+        var seriesDetail = "\n                <a class=\"results__aside\" href=\"./details.html?id=".concat(tvSeriesId, "\">\n                    <img data-image=\"").concat(index, "\" class=\"popular__image\" src=").concat(tvSeriesImage, " crossOrigin =\"Anonymous\";>\n                    <div data-search-details=\"").concat(index, "\" class=\"popular__details\">\n                        <h2 data-title class=\"popular__title\">").concat(tvSeriesName, "</h2>\n                        <div class=\"popular__imdb\">\n                            ").concat(svg, "\n                            <span data-imdb class=\"popular__imdbpoint\">").concat(tvSeriesImdb, "</span>\n                        </div>\n                        <p data-genres class=\"popular__genres\">").concat(tvSeriesGenres, "</p>\n                    </div>\n                </div>\n                ");
+        resultsItem.insertAdjacentHTML("beforeend", seriesDetail);
+        var imageElement = document.querySelectorAll("[data-image]");
+        var lastImageElement = imageElement[imageElement.length - 1];
+        colorImage(lastImageElement);
+        index++;
+        resultContent.innerHTML = "".concat(index, " results found for the word ").concat(seriesName);
+      } catch (error) {}
+    });
+  }).catch(function (error) {
+    var errorHandle = "\n        <p class=\"error__handle\">".concat(error, "</p>\n        ");
+    resultsItem.insertAdjacentHTML("beforeend", errorHandle);
+  });
+};
+
+var params = new URLSearchParams(window.location.search);
+var searchTvSeries = params.has('series');
+
+if (searchTvSeries) {
+  var seriesName = params.get('series');
+  searchBarText(seriesName);
+  showResults(seriesName);
+}
+
+function colorImage(image) {
+  var imageIndex = image.getAttribute('data-image');
+  var colorthief = new ColorThief();
+  image.addEventListener('load', function () {
+    var rgb = colorthief.getColor(image);
+    var red = rgb[0];
+    var green = rgb[1];
+    var blue = rgb[2];
+    var lastImageDetails = document.querySelector("[data-search-details=\"".concat(imageIndex, "\"]"));
+    lastImageDetails.style.backgroundColor = "rgb(".concat(red, ",").concat(green, ",").concat(blue, ")");
+  });
+}
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49426" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","searchpage/results.js"], null)
+//# sourceMappingURL=/results.c828268b.js.map
